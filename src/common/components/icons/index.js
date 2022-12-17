55c6e46f91495a725790656d3e1c5d9a8a0c61b7
NKateryna <krapiva.b.s@gmail.com>
2022-12-17 23:32:26 +0200

    Add typography and icons

---
+++ src/common/components/icons/index.js
@@ -0,0 +1,39 @@
+export { LogoIcon } from './logo';
+export { BackgroundIcon } from './background';
+/**loading */
+export { LoadingV11Icon } from './loading-v1';
+export { LoadingV2Icon } from './loading-v2';
+export { LoadingV3Icon } from './loading-v3';
+export { LoadingV4Icon } from './loading-v4';
+/*sidbar*/
+export { AddChannelIcon } from './add-channel';
+export { DownloadAppIcon } from './download-app';
+export { ChannelSearchIcon } from './channel-search';
+/*acc manipulation*/
+export { HeadphonesIcon } from './headphones.js';
+export { HeadphonesOffIcon } from './headphones-off';
+export { MicrophoneIcon } from './microphone';
+export { MicrophoneOffIcon } from './microphone-off';
+export { SettingsIcon } from './settings';
+/*status*/
+export { StatusActiveIcon } from './status-active';
+export { StatusDoNotDisturbIcon } from './status-do-not-disturb';
+export { StatusInvisibleIcon } from './status-invisible';
+export { StatusInactiveIcon } from './status-inactive';
+/*navigation*/
+export { FrendsIcon } from './frends';
+export { NitroIcon } from './nitro';
+export { CreateMessagesIcon } from './create-messages.js';
+export { CloseChatIcon } from './сlose-chat';
+/*content*/
+export { NewGroupChatIcon } from './new-group-chat.js';
+export { MailIcon } from './mail';
+export { HelpIcon } from './help';
+export { SearchIcon } from './search';
+export { MoreIcon } from './more';
+export { MessageIcon } from './message';
+export { DenyIcon } from './deny';
+export { AcceptIcon } from './accept';
+export { UnblockUserIcon } from './unblock-user.js';
+
+export { CreateServerIcon } from './create-server';
---
+++ src/common/components/icons/new-group-chat.js
@@ -0,0 +1,25 @@
+export function NewGroupChatIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_2_263)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M20.998 0V3H23.998V5H20.998V8H18.998V5H15.998V3H18.998V0H20.998ZM2.99805 20V24L8.33205 20H14.998C16.102 20 16.998 19.103 16.998 18V9C16.998 7.896 16.102 7 14.998 7H1.99805C0.894047 7 -0.00195312 7.896 -0.00195312 9V18C-0.00195312 19.103 0.894047 20 1.99805 20H2.99805Z"
+          fill="#DCDDDE"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_2_263">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
