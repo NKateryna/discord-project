b768845595efeeb16837c681df4c406efe06613d
Kateryna <84880408+NKateryna@users.noreply.github.com>
2022-12-18 20:01:38 +0200

    Add typography and icons (#7)

---
+++ src/common/components/icons/create-messages.js
@@ -0,0 +1,16 @@
+export function CreateMessagesIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.3334 6.88888H6.88891V11.3333H5.11113V6.88888H0.666687V5.1111H5.11113V0.666656H6.88891V5.1111H11.3334V6.88888Z"
+        fill="#B9BBBE"
+      />
+    </svg>
+  );
+}
