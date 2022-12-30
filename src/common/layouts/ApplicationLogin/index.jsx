b6c9f2e2d12eaef33a0ded9ac00a675e2f1e204c
NKateryna <krapiva.b.s@gmail.com>
2022-12-31 01:30:58 +0200

    fix comments

--- src/common/layouts/ApplicationLogin/index.jsx
+++ src/common/layouts/ApplicationLogin/index.jsx
@@ -3,22 +3,20 @@ import { BackgroundIcon } from './../../components/icons/index';
 
 function ApplicationLogin({ children, title, subtitle, helpText, link }) {
   return (
-    <>
-      <div className={styles.container}>
-        <BackgroundIcon />
-        <section className={styles.box}>
-          <header>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.subtitle}>{subtitle}</p>
-          </header>
-          <form>{children}</form>
-          <div>
-            <span className={styles.helpText}>{helpText}</span>
-            {link}
-          </div>
-        </section>
-      </div>
-    </>
+    <div className={styles.container}>
+      <BackgroundIcon />
+      <section className={styles.box}>
+        <header>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.subtitle}>{subtitle}</p>
+        </header>
+        <form>{children}</form>
+        <div>
+          <span className={styles.helpText}>{helpText}</span>
+          {link}
+        </div>
+      </section>
+    </div>
   );
 }
 
