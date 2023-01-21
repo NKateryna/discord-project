7d57d8f858e9814126a9577fe8af80efef00b219
NKateryna <krapiva.b.s@gmail.com>
2023-01-21 20:10:10 +0200

    Loading screen component

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,29 +2,37 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Dashboard from '../pages/dashboard';
 import Nitro from '../pages/dashboard/nitro';
-import Loading from '../pages/loading';
+import LoadingScreen from '../common/components/LoadingScreen';
 import Login from '../pages/login';
 import Register from '../pages/register';
 import FriendsRouters from './routes-friends';
 
 function AppRoutes() {
   return (
-    <BrowserRouter>    
-        <Routes>
-          <Route path="/*">
-            <Route index element={<Loading />} />
-            <Route path="login/" element={<Login />} />
-            <Route path="register/" element={<Register />} />
-            <Route path="dashboard/*">
-              <Route index element={<Dashboard />} />
-              <Route path="friends/*">
-                <Route index element={<Navigate replace to="online" />} />
-                <Route path="*" element={<FriendsRouters />} />
-              </Route>
-              <Route path="nitro/" element={<Nitro />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*">
+          <Route
+            index
+            element={
+              <LoadingScreen
+                title="DID YOU KNOW"
+                subtitle="We’re glad you’re back!"
+              />
+            }
+          />
+          <Route path="login/" element={<Login />} />
+          <Route path="register/" element={<Register />} />
+          <Route path="dashboard/*">
+            <Route index element={<Dashboard />} />
+            <Route path="friends/*">
+              <Route index element={<Navigate replace to="online" />} />
+              <Route path="*" element={<FriendsRouters />} />
             </Route>
+            <Route path="nitro/" element={<Nitro />} />
           </Route>
-        </Routes>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
