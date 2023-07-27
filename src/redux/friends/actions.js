18fee335530bbebe22d62150fd744bdf4965b3f4
NKateryna <krapiva.b.s@gmail.com>
2023-07-27 16:26:37 +0300

    hotfix

--- src/redux/friends/actions.js
+++ src/redux/friends/actions.js
@@ -2,7 +2,6 @@ const actions = {
   CREATING_FRIENDS_LIST: 'CREATING_FRIENDS_LIST',
   REMOVE_FRIENDS_LIST: 'REMOVE_FRIENDS_LIST',
   TOGGLE_LOADING: 'TOGGLE_LOADING',
-  // SAVE_ACTIVE_ITEM: 'SAVE_ACTIVE_ITEM',
 };
 
 export default actions;
@@ -134,7 +133,7 @@ export const acceptFriendInvitation =
 export const declineFriendInvitation =
   (navigate, cookies, friendId, pageName) => async (dispatch) => {
     const token = cookies.get('authToken', { path: '/' });
-    console.log(friendId);
+
     if (!token) {
       navigate('login');
     } else {
