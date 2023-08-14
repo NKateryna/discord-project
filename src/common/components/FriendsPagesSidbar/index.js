4c7cb572ccf7eaa1b72415d9d89dd651228abe73
NKateryna <krapiva.b.s@gmail.com>
2023-08-14 18:56:10 +0300

    Empty state for Active section

--- src/common/components/FriendsPagesSidbar/index.js
+++ src/common/components/FriendsPagesSidbar/index.js
@@ -1,10 +1,22 @@
 import styles from './index.module.css';
 
-function FriendsPagesSidbar({ title, children }) {
+function FriendsPagesSidbar() {
+  const activeFriends = null;
+
   return (
     <div className={styles.friendsSidbar}>
-      <div className={styles.title}>{title}</div>
-      {children}
+      <div className={styles.friendsSidbar__title}>Active Now</div>
+      {!activeFriends ? (
+        <div className={styles.emptyStateBox}>
+          <div className={styles.emptyStateBox__title}>
+            It’s quiet for now...
+          </div>
+          <div className={styles.emptyStateBox__text}>
+            When a friend starts active-now activity - like playing a <br />
+            game or hanging out on voice - we’ll show it here!
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
