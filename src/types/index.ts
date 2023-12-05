0b114bd6d3bc22a3e4c976f18bd66d205df53b33
Novik Kateryna <novik.katerina.96@gmail.com>
2023-12-06 00:13:06 +0200

    delete coments

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,19 +10,7 @@ export type PropsWithChildren = {
 };
 export type ReactNodeElement = ReactNode;
 export type JSXElement = ReactElement;
+
 export type CSSProps = CSSProperties;
 
 export type OnClickButton = MouseEventHandler<HTMLButtonElement>;
-
-// Можно будет разширять и прокидывать дополнительные пропсы
-
-//"form:" event: React.FormEvent
-
-//"change" event: React.KeyboardEvent<от какого элемента идёт событие, пример:HTMLInputElement>
-// React.MouseEventHandler<>
-
-//enum для статусов
-
-// : { className: string }
-
-interface Props {}
